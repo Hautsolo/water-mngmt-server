c2763ab78d230afba6d251f9e6bc5660e24ddaab
braydenbubp <braydenbubp1@gmail.com>
2024-07-10 21:06:49 -0400

    setup

---
+++ watermgmtapi/views/auth.py
@@ -0,0 +1,30 @@
+from watermgmtapi.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['POST'])
+def check_user(request):
+    '''Checks to see if User has Associated Gamer
+
+    Method arguments:
+      request -- The full HTTP request object
+    '''
+    uid = request.data['uid']
+
+    # Use the built-in authenticate method to verify
+    # authenticate returns the user object or None if no user is found
+    user = User.objects.filter(uid=uid).first()
+
+    # If authentication was successful, respond with their token
+    if user is not None:
+        data = {
+            'id': user.id,
+            'first_name': user.first_name,
+            'last_name': user.last_name
+        }
+        return Response(data)
+    else:
+        # Bad login details were provided. So we can't log the user in.
+        data = { 'valid': False }
+        return Response(data)
